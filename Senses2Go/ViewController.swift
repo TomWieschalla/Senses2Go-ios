4f70fa101239f49e9fec7289d2e0217602813df1
Tom Wieschalla <tom.wieschalla@adesso.de>
2016-11-22 15:46:01 +0100

    Initial Commit

---
+++ Senses2Go/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Senses2Go
+//
+//  Created by Admin on 22.11.16.
+//  Copyright © 2016 Adesso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
